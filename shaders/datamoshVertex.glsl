8b1f6ccbafb35e1187ae99a6eaedc0b661acc697
catartix-rgb <catartix@gmail.com>
2026-06-04 14:12:55 -0600

    Delete shaders directory

--- shaders/datamoshVertex.glsl
+++
@@ -1,9 +0,0 @@
-// datamoshVertex.glsl
-// Pass-through for a fullscreen-ish quad. UV is preserved for fragment work.
-
-varying vec2 vUv;
-
-void main() {
-  vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
